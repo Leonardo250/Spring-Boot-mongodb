373f12d4f92c60589e28869a1f720b72264d0ee2
Leonardo250 <leomoulin2002@gmail.com>
2025-11-07 01:27:45 -0300

    Primeiro commit

---
+++ src/main/java/com/leo/srpingmongodb/SrpingmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.leo.srpingmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingmongodbApplication.class, args);
+	}
+
+}
