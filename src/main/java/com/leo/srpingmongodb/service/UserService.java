e7e12c60e4448db65fd1ace06538fd1d6b6ed459
Leonardo250 <leomoulin2002@gmail.com>
2025-11-07 22:25:15 -0300

    Conecxão estabelecida com o mongodb

---
+++ src/main/java/com/leo/srpingmongodb/service/UserService.java
@@ -0,0 +1,19 @@
+package com.leo.srpingmongodb.service;
+
+import com.leo.srpingmongodb.domain.User;
+import com.leo.srpingmongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+}
