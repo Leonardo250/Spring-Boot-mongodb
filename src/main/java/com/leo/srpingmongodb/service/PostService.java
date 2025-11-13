791dea8a3527eb7d4cb60a7cc5942cd55f92a192
Leonardo250 <leomoulin2002@gmail.com>
2025-11-12 22:31:53 -0300

    Consulta simples com @Query

--- src/main/java/com/leo/srpingmongodb/service/PostService.java
+++ src/main/java/com/leo/srpingmongodb/service/PostService.java
@@ -23,7 +23,7 @@ public class PostService {
     }
 
     public List<Post> findByysTitle(String text){
-        return repo.findByTitleContainingIgnoreCase(text);
+        return repo.searchTitle(text);
     }
 
 
